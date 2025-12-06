b8478a78b68d711ad3316d19f83f22e5d6eda6eb
polymorpheteen <138623315+polymorpheteen@users.noreply.github.com>
2025-12-06 19:35:49 +0100

    Adjust the homepage content and apply global css reset for consistent styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <></>
-}
+import ProfileCard from './components/ProfileCard'
+import './styles/App.css'
 
-export default App
+export default function App() {
+  return (
+    <main>
+      <div className="main-grid">
+        <ProfileCard />
+      </div>
+    </main>
+  )
+}
